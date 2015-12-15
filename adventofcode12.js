b53243ec0a544735f4ef2404522b9ec8aa86b2b0
btcpbordeaux <pbordeaux@belltec.com>
2015-12-15 10:48:01 -0600

    day 12 part two, strugg city, pop: me

--- adventofcode12.js
+++ adventofcode12.js
@@ -14,19 +14,49 @@ console.log(arr.reduce((s,n) => Number(s) + Number(n), 0));
 var t = eval(document.querySelector('pre').textContent);
 
 var adventDay12 = function (obj) {
-  var getKeys = function (o) {return Object.keys(o)},
-      check = function (o) {
-        var keys = getKeys(o);
+  var count = 0;
+  var getKeys = function (o) {
+    if (typeof o == 'object') {
+      return Object.keys(o);
+      } 
+    else {
+      return false;
+      }
+    },
+    getVals = function (o) {
+      var ret = [], keys = getKeys(o);
+      if (keys) {
         for (i in keys) {
-          if (!isNaN(Number(i))) {
-              if (o(j) == 'red') {
-                return 0;
-              }
-            }
-          } else if (isNaN(Number(i))) {
+          ret.push(o[i]);
+        }
+      } else if (!keys) {
+        return false;
+      }
+    },
+    getNums = function (s) {
+      if (isNaN(Number(s))) {
+        ret +=0
+      } else if (!isNaN(Number(s))) {
+        ret += Number(s)
+      }
+    },
+    check = function (o) {
+      var keys = getKeys(o);
+        if (keys) { 
+            if (isNaN(Number(keys[0]))) {
+              var vals = getVals(o);
+              
+              if (vals.every(e => e != 'red')) {
+                vals.map(check);
+              } else {delete o;}
 
-          }
+            } else if (!isNaN(Number(keys[0]))) {
+              var vals = getVals(o);
+                vals.map(check);      
+              }   
+        } else if (!keys) {
+          getNums(o);
         }
       }
-      check(obj);  
+    check(obj);  
 }
