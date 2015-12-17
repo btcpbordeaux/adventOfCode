540a8518b13d5c622fbf09d9091abaa6ac880543
charles bordeaux <charles@pbordeaux.local>
2015-12-17 00:11:52 -0600

    day 8 part 2

--- adventofcode8.js
+++ adventofcode8.js
@@ -9,3 +9,11 @@ console.log(count.reduce( (p,n) => p+n,0));
 
 
 //part two
+var count2 = new Array(t.length).fill(0),
+	r = new RegExp(/\\/g),
+	l = new RegExp(/\"/g);
+for ( i in t) {t[i] = t[i].replace(r,'\\\\')};
+for ( i in t) {t[i]= t[i].replace(l,'\\"')};
+for (i in t) {t[i] = '"'+t[i]+'"'};
+for (i in t) {count2[i] += t[i].length - eval(t[i]).length};
+console.log(count2.reduce( (p,n) => p+n,0));
